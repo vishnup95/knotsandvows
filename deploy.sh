818cc6c59ba31c194d14c7725555ef07d5249c2b
Developer One <dev1exypnos@gmail.com>
2019-02-26 11:17:48 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #/bin/bash
-set -e
+rm -rf consumerportal
+export PATH=$PATH:/usr/local/bin
 source ~/.profile
-git clone git@bitbucket.org:exypnosdev/consumerportal.git
+export NODE_PATH=/home/staging/.nvm/versions/node/v10.9.0/bin/node
+export USER=staging
+export HOME=/home/staging
+source $HOME/.nvm/nvm.sh
+nvm use v10.9.0
+set -e
+git clone -b staging git@bitbucket.org:exypnosdev/consumerportal.git
 cd consumerportal
+mv sample-env.txt .env
+npm install
+npm run build
+pm2 restart npm -- run start-prod --
