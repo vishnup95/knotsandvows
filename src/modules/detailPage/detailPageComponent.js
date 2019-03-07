0713bfc294bdd0a3fe10c29d7b25ea2955eafbfd
Dev One <dev1exypnos@gmail.com>
2019-03-07 20:30:31 +0530

    product detail page ui updates

--- src/modules/detailPage/detailPageComponent.js
+++ src/modules/detailPage/detailPageComponent.js
@@ -11,6 +11,9 @@ import JumbotronComponent from '../../components/Jumbotron/jumbotron';
 import ReviewItem from '../../components/Reviews/reviews';
 import ReactPaginate from 'react-paginate';
 import ProductGallery from '../../modals/productGallery/GalleryModal';
+import StarRating from '../../components/StarRating/starRating';
+import { imagePath } from '../../utils/assetUtils';
+import TalkToWeddingPlanner from '../../components/TalkToWeddingPlanner/talkToWeddingPlanner';
 
 const mapStateToProps = state => ({
     user: state.session.user,
@@ -34,7 +37,7 @@ class DetailPageComponent extends Component {
             showGallery: false,
             vendor: '',
             category: '',
-            reviewPage : 1
+            reviewPage: 1
         };
     }
 
@@ -64,7 +67,7 @@ class DetailPageComponent extends Component {
         console.log(this.props.match);
         let category = this.props.match.params.category_name;
         let vendor = this.props.match.params.vendor_name;
-        this.setState({ vendor: vendor, category: category, reviewPage : 1});
+        this.setState({ vendor: vendor, category: category, reviewPage: 1 });
         this.props.dispatch(actions.fetchVendorDetails(category, vendor));
         this.props.dispatch(actions.fetchSimilarVendors(category, vendor));
         this.props.dispatch(actions.fetchReviews(category, vendor, 1));
@@ -96,7 +99,7 @@ class DetailPageComponent extends Component {
 
         const availableAmenities = amenities.map((amenity, index) => {
 
-            return <li key={index}>{amenity.name}</li>
+            return <li key={index} ><span className={style.listIcon} style={{ background: "url(" + amenity.icon_url + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}></span>{amenity.name}</li>
 
         });
         return availableAmenities;
@@ -120,9 +123,9 @@ class DetailPageComponent extends Component {
         return jumbotronData;
     }
 
-    pageChangeHandler(data){
-        this.props.dispatch(actions.fetchReviews(this.state.category, this.state.vendor, data.selected+1));
-        this.setState({reviewPage: data.selected+1});
+    pageChangeHandler(data) {
+        this.props.dispatch(actions.fetchReviews(this.state.category, this.state.vendor, data.selected + 1));
+        this.setState({ reviewPage: data.selected + 1 });
     }
 
     render() {
@@ -138,18 +141,25 @@ class DetailPageComponent extends Component {
                         <div className={style.detailSection}>
                             <Row className={style.infoBox}>
                                 <div>
-                                    <h3 >{details.name}</h3>
+                                    <h3 >{details.name} <img src={imagePath('wishlist_unselected.svg')} className={style.heartImg} alt="Unselected heart" />
+                                    </h3>
                                     <p >{details.city} (<a href="/">View on Map</a>)</p>
                                     <p >{details.address}</p>
                                 </div>
-                                <div>
-                                    star rating
-                            {/* <div className={style.rating}>4.5/5 rating</div> */}
-                                    <div className={style.review}> {details.reviews_count} reviews</div>
+                                <div className={style.infoSub}>
+                                    <div className={style.ratingWrap}>
+                                        <div>
+                                            <StarRating rating={details.rating} size={'large'} />
+                                        </div>
+                                        {/* <div className={style.rating}>4.5/5 rating</div> */}
+                                        <div className={style.review}> {details.reviews_count} Reviews</div>
+                                    </div>
+                                    <div className={style.viewBtnWrap}>
+                                        <button className={style.viewBtn}>Add to wishlist</button>
+                                        <button className={style.removeBtn}>Remove from wishlist</button>
+                                    </div>
                                 </div>
-                                <div className={style.viewBtnWrap}>
-                                    <button className={style.viewBtn} onClick={() => this.toggleGallery()}>View Gallery</button>
-                                </div>
+
                             </Row>
                             <Row className={style.detailNav}>
 
@@ -171,7 +181,7 @@ class DetailPageComponent extends Component {
                                     }
                                     <li>Reviews</li>
                                     {details.gallery && details.gallery.length > 0 &&
-                                        <li>Gallery ({details.gallery.length} Photos)</li>
+                                        <li><button onClick={() => this.toggleGallery()}>Gallery ({details.gallery.length} Photos)</button></li>
                                     }
                                 </ul>
 
@@ -219,13 +229,43 @@ class DetailPageComponent extends Component {
                                             <MapComponent lat={Number(details.location.latitude)} lng={Number(details.location.longitude)}></MapComponent>
                                         </Col>
                                     }
+                                    {reviewsData && reviewsData.results && reviewsData.results.length > 0 &&
+                                        <Col md="12" className={style.detailSubSection}>
+                                            <div className={style.reviewHeader}>Reviews <span>({reviewsData.total_review_count})</span></div>
+                                            <div className={style.starWrap}>
+                                                <StarRating rating={details.rating} size={'large'} />
+                                            </div>
+                                            {
+                                                reviewsData.results.map((review, index) => {
+                                                    return (
+                                                        <ReviewItem review={review} key={index} />
+                                                    );
+                                                })
+                                            }
+                                            {reviewsData.no_of_pages > 1 &&
+                                                <ReactPaginate
+                                                    previousLabel={'<'}
+                                                    nextLabel={'>'}
+                                                    breakLabel={'...'}
+                                                    forceSelect={this.state.reviewPage}
+                                                    breakClassName={'break-me'}
+                                                    pageCount={reviewsData.no_of_pages}
+                                                    marginPagesDisplayed={2}
+                                                    pageRangeDisplayed={5}
+                                                    onPageChange={(data) => this.pageChangeHandler(data)}
+                                                    containerClassName={'pagination'}
+                                                    subContainerClassName={'pages pagination'}
+                                                    activeClassName={'active'} />
+                                            }
+                                        </Col>
+                                    }
                                 </Col>
+
                                 <Col md="5">
                                     <Col md="12" className={`${style.detailSubSection} ${style.rightSection}`}>
                                         <Col md="12" className={`${style.rightSubSection} ${style.sectionBorder}`}>
                                             <h3>The Club</h3>
-                                            <p className={style.venueDesc}>D N Nagar32, Cosmopolitan Education Society Marg,
-Shanti Nagar, Andheri, Mumbai.</p>
+                                            <p className={style.venueDesc}>D N Nagar32, Cosmopolitan Education Society Marg, Shanti Nagar, Andheri, Mumbai.</p>
                                         </Col>
                                         <Col md="12" className={`${style.rightSubSection} ${style.availableArea} ${style.sectionBorder}`}>
                                             <div>
@@ -287,9 +327,6 @@ Shanti Nagar, Andheri, Mumbai.</p>
 
                                         </Col>
                                         <Col md="12" className={style.rightSubSection}>
-                                            <button className={style.addToCart} onClick={this.addToWishlist}>Add to Wishlist</button>
-                                        </Col>
-                                        <Col md="12" className={style.rightSubSection}>
                                             {details.cancelation_policy_url &&
                                                 <a href={details.cancelation_policy_url} >+ Cancellation policy</a>
                                             }
@@ -298,36 +335,52 @@ Shanti Nagar, Andheri, Mumbai.</p>
                                             }
                                         </Col>
                                     </Col>
-                                </Col>
-                            </Row>
-                            <Row>
-                                {reviewsData && reviewsData.results && reviewsData.results.length > 0 &&
-                                    <Col md="12" className={style.detailSubSection}>
-                                        <div className={style.reviewHeader}>Reviews <span>({reviewsData.total_review_count})</span></div>
-                                        {
-                                            reviewsData.results.map((review, index) => {
-                                                return (
-                                                    <ReviewItem review={review} key={index} />
-                                                );
-                                            })
-                                        }
-                                        { reviewsData.no_of_pages > 1 &&
-                                         <ReactPaginate
-                                            previousLabel={'<'}
-                                            nextLabel={'>'}
-                                            breakLabel={'...'}
-                                            forceSelect={this.state.reviewPage}
-                                            breakClassName={'break-me'}
-                                            pageCount={reviewsData.no_of_pages}
-                                            marginPagesDisplayed={2}
-                                            pageRangeDisplayed={5}
-                                            onPageChange={(data) => this.pageChangeHandler(data)}
-                                            containerClassName={'pagination'}
-                                            subContainerClassName={'pages pagination'}
-                                            activeClassName={'active'} />
-                                        }
+                                    <Col className={style.detailSubSection}>
+                                        <Col md="12" className={`#{style.rightSubSection} text-center`}>
+                                            <p className={style.needHelp}>Need some guidance on selecting vendors?</p>
+                                            {/* <button className={style.addToCart} onClick={this.addToWishlist}>Add to Wishlist</button> */}
+                                            <TalkToWeddingPlanner buttonText={'Talk to our wedding planner!'} />
+                                        </Col>
                                     </Col>
-                                }
+                                    {false &&
+                                        <Col className={`${style.detailSubSection} ${style.noteSection}`}>
+                                            <Col md="12" className={`${style.rightSubSection} text-left`}>
+                                                <h4 className={style.noteHeader}>Notes</h4>
+                                                <div className={style.noteWrap}>
+                                                    <div>
+                                                        <span className={style.noteTitle}>Binu</span>
+                                                        <span className={style.noteDate}>07 Mar 2019</span>
+                                                    </div>
+                                                    <div className={style.noteText}>
+                                                        <div>
+                                                            <span className="edit-icon"></span>
+                                                            <span className="delete-icon"></span>
+                                                        </div>
+                                                        <div>
+                                                            Viverra accumsan in nisl nisi scelerisque. Sit amet justo donec enim. Commodo elit at imperdiet dui accumsan sit amet. Eget aliquet nibh praesent tristique magna. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Morbi leo urna molestie at elementum eu facilisis sed.
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                                <div className={style.noteWrap}>
+                                                    <div>
+                                                        <span className={style.noteTitle}>Binu</span>
+                                                        <span className={style.noteDate}>07 Mar 2019</span>
+                                                    </div>
+                                                    <div className={style.noteText}>
+                                                        <div>
+                                                            <span className="edit-icon"></span>
+                                                            <span className="delete-icon"></span>
+                                                        </div>
+                                                        <div>
+                                                            Viverra accumsan in nisl nisi scelerisque. Sit amet justo donec enim. Commodo elit at imperdiet dui accumsan sit amet. Eget aliquet nibh praesent tristique magna. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Morbi leo urna molestie at elementum eu facilisis sed.
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                            </Col>
+                                        </Col>
+                                    }
+                                </Col>
+
                             </Row>
                         </div>
                         <div>
@@ -335,7 +388,7 @@ Shanti Nagar, Andheri, Mumbai.</p>
                     </div>
                 }
                 <Modal isOpen={this.state.showGallery} toggle={() => this.toggleGallery()} centered={true}>
-                   <ProductGallery details={details} close={this.toggleGallery}></ProductGallery>
+                    <ProductGallery details={details} close={this.toggleGallery}></ProductGallery>
 
                 </Modal>
                 {details && this.props.similarVendors && this.props.similarVendors.length > 0 &&
