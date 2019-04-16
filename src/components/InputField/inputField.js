e21551122f829f20a56ed452c9f3d8eca1b8fe08
Dev One <dev1exypnos@gmail.com>
2019-04-16 12:23:34 +0530

    talk to ahwanam fixes

--- src/components/InputField/inputField.js
+++ src/components/InputField/inputField.js
@@ -116,7 +116,7 @@ class InputField extends Component {
             <div className='input-field floating-label'>
                 {
                     this.props.id === 'comments' ? 
-                    <textarea className='input-box' rows="4"
+                    <textarea className='input-box' rows="1"
                         type={this.props.type} id={this.props.id} required={this.props.required}
                         pattern={this.props.pattern || defaultPatterns[this.props.type]}
                         onFocus={() =>  this.handleFocus(event.target)} 
@@ -142,7 +142,7 @@ class InputField extends Component {
                     />
                 }
               
-                <Label className={`${this.props.type === 'date' ? 'date-placeholder' : 'input-placeholder'} ${this.props.id === 'comments' ? 'comment-placeholder' : ''}`} 
+                <Label className={`${this.props.type === 'date' ? 'date-placeholder' : 'input-placeholder'} ${this.props.id === 'comments' ? 'input-placeholder' : ''}`} 
                     htmlFor={this.props.id}>
                   {this.props.placeHolder}
                 </Label>
