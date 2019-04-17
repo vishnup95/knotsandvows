4b6ec813a975ad4d8ee1fc60e4861f8f2a4bb857
Developer One <dev1exypnos@gmail.com>
2019-04-17 10:05:01 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 rm -rf consumerportal
 export PATH=$PATH:/usr/local/bin
 source ~/.profile
-export NODE_PATH=/home/sandbox/.nvm/versions/node/v10.9.0/bin/node
-export USER=sandbox
-export HOME=/home/sandbox
+export NODE_PATH=/home/sevenvows/.nvm/versions/node/v10.9.0/bin/node
+export USER=sevenvows
+export HOME=/home/sevenvows
 source $HOME/.nvm/nvm.sh
 nvm use v10.9.0
 set -e
