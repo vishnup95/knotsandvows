15be051ebcf7a07ef3ab69cb35e3d0961d01012f
Dev One <dev1exypnos@gmail.com>
2019-04-24 12:36:35 +0530

    staging and prod deply branch updated

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -2,13 +2,13 @@
 rm -rf consumerportal
 export PATH=$PATH:/usr/local/bin
 source ~/.profile
-export NODE_PATH=/home/sandbox/.nvm/versions/node/v10.9.0/bin/node
-export USER=sandbox
-export HOME=/home/sandbox
+export NODE_PATH=/home/sevenvows/.nvm/versions/node/v10.9.0/bin/node
+export USER=sevenvows
+export HOME=/home/sevenvows
 source $HOME/.nvm/nvm.sh
 nvm use v10.9.0
 set -e
-git clone -b sandbox git@bitbucket.org:exypnosdev/consumerportal.git
+git clone -b master git@bitbucket.org:exypnosdev/consumerportal.git
 cd consumerportal
 mv prod-env.txt .env
 npm install
