f30872e43c122e1ceb61bb39efa6e22ff53b51d1
Developer Two <dev2exypnos@gmail.com>
2019-04-29 11:08:00 +0530

    Launch tickets in trello fixes

--- src/modules/ceremonyDetail/CeremonyDetail.js
+++ src/modules/ceremonyDetail/CeremonyDetail.js
@@ -61,6 +61,7 @@ class CeremonyDetail extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    window.scrollTo(0,0);
     if (nextProps.ceremonyDetails !== null) {
       let filteredCategories = nextProps.ceremonyDetails.categories.filter(item => {
         return item.vendors !== null && item.vendors.length > 0
