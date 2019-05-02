7ab103951ac596d0aa12dca28af25aa1961d6dce
Dev One <dev1exypnos@gmail.com>
2019-05-02 19:44:11 +0530

    gold and ruby price fix mobile

--- src/modules/packages/Ruby/rubyPackage.js
+++ src/modules/packages/Ruby/rubyPackage.js
@@ -251,9 +251,9 @@ class RubyPackage extends Component {
               <div className={`${styles.offerPrice} `}>
                 <img className={styles.offerPriceImg} src={imagePath('offer-bg-ruby.png')} alt="offer" />
                 <div className={styles.offer}>Price starting at</div>
-                <div className={`${styles.originalStrike} ${styles.ruby}`}>Original Price  ₹2,390,000</div>
-                <div className={`${styles.original} ${styles.ruby}`}>₹2,150,000</div>
-                <div className={styles.save}>You Save<br />₹2.5 Lakhs</div>
+                <div className={`${styles.originalStrike} ${styles.ruby}`}>Original Price  ₹58,00,000</div>
+                <div className={`${styles.original} ${styles.ruby}`}>₹51,00,000</div>
+                <div className={styles.save}>You Save<br />₹7 Lakhs</div>
               </div>
             </div>
           </Row>
