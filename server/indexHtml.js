61bdc03f4627ca220958b991a8276c11b34aa324
Dev One <dev1exypnos@gmail.com>
2019-05-07 17:39:00 +0530

    google font added

--- server/indexHtml.js
+++ server/indexHtml.js
@@ -68,6 +68,7 @@ export const indexHtml = ({ helmet, initialState, markup, bundles }) => {
         <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre:400,500,700|Roboto:300,400,500,700" rel="stylesheet">
         ${cssLinks()}
         ${helmet.style.toString()}
         ${helmet.script.toString()}
