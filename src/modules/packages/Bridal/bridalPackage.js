3a9cbd731ec8a722935a0ab920e8211de648ccb4
Dev One <dev1exypnos@gmail.com>
2019-06-14 15:50:12 +0530

    banner image and mobile ui fixes

--- src/modules/packages/Bridal/bridalPackage.js
+++ src/modules/packages/Bridal/bridalPackage.js
@@ -27,7 +27,7 @@ class BridalPackage extends Component {
           <div className={styles.bridalImg}>
             <div className={styles.coverDetail}>
               {/* <h4>Planning your</h4> */}
-              <h1>Because in your dreams, every detail matters</h1>
+              <h1>Because in your dreams,<br/>every detail matters</h1>
             </div>
           </div>
         </div>
@@ -35,7 +35,7 @@ class BridalPackage extends Component {
         <div className={styles.containerClass}>
           <Row className={`${styles.detailBox}`}>
             <Col md="12">
-              <h2 className={styles.pink}>It&#39;s your wedding and Knot&Vows wants your day to be as perfect as you are.</h2>
+              <h2 className={styles.pink}>It&#39;s your wedding and Knots&Vows wants your day to be as perfect as you are.</h2>
             </Col>
           </Row>
 
@@ -45,7 +45,7 @@ class BridalPackage extends Component {
             })
           }
           <Row className={`${styles.detailBox}`}>
-            <Col md="6" className="text-right flex justify-center">
+            <Col md="6" className={`${styles.priceCol} text-right flex`}>
               <div className={styles.priceWrap}>
                 <img className={styles.priceBg} src={imagePath('bridal-offer-bg.svg')} alt="" />
                 <div className={styles.price}>Price starting</div>
@@ -53,7 +53,7 @@ class BridalPackage extends Component {
                 <div className={styles.priceValue}>6 Lakhs</div>
               </div>
             </Col>
-            <Col md="6" className="flex flex-column justify-center mb-5">
+            <Col md="6" className={`${styles.talkWrap} flex flex-column justify-center mb-5`}>
               <TalkToWeddingPlanner buttonText={'Let us help you!'} />
             </Col>
           </Row>
