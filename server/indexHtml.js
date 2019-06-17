382f9ab7753a6a138edf573915733eb64429dee4
Dev Two <dev2exypnos@gmail.com>
2019-06-17 16:17:57 +0530

    Code changes for chat to deploy in production

--- server/indexHtml.js
+++ server/indexHtml.js
@@ -97,16 +97,7 @@ export const indexHtml = ({ helmet, initialState, markup, bundles }) => {
         })(window,document,'script','dataLayer','${gtmId}');</script>
         <!-- End Google Tag Manager -->
         <!-- Pure Chat -->
-        <script type='text/javascript' data-cfasync='false'>
-        window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; 
-        (function () { var done = false; var script = document.createElement('script'); 
-        script.async = true; script.type = 'text/javascript'; 
-        script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; 
-        document.getElementsByTagName('HEAD').item(0).appendChild(script); 
-        script.onreadystatechange = script.onload = function (e) 
-        { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) 
-        { var w = new PCWidget({c: '93716f8f-05de-4275-a033-aa244d31b6d8', f: true }); 
-        done = true; } }; })();</script>
+        <script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: '3f83888a-3ba2-428c-ad79-8081687d11ad', f: true }); done = true; } }; })();</script>
         <script>
         window.onload = function() {
           setTimeout(function(){ 
